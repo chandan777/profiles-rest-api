565b9d87638d6fa65c730a59aed61696c872e77c
Chandan <chandan.sidana@gmail.com>
2019-11-10 16:48:51 +0530

    ..

--- profiles_api/models.py
+++ profiles_api/models.py
@@ -23,7 +23,7 @@ class UserProfileManager(BaseUserManager):
         """Create and save a new superuser with given detail"""
         user=self.create_user(email,name,password)
         user.is_superuser=True
-        user.is_staff=True 
+        user.is_staff=True
         user.save(using=self._db)
         return user
 
@@ -50,3 +50,23 @@ class UserProfile(AbstractBaseUser,PermissionsMixin):
     def __str__(self):
         """Return string representation of our user"""
         return self.email
+
+
+
+
+class Policy(models.Model):
+    Policy_id=models.AutoField(primary_key=True)
+    Date_Of_Purchase=models.CharField(max_length=255)
+    Customer_id=models.IntegerField()
+    Fuel=models.CharField(max_length=255)
+    Vehicle_Segment=models.CharField(max_length=255)
+    Premium=models.FloatField()
+    bodily_injury_liability=models.BooleanField(default=False)
+    personal_injury_protection=models.BooleanField(default=False)
+    property_damage_liability=models.BooleanField(default=False)
+    collision=models.BooleanField(default=False)
+    comprehensive=models.BooleanField(default=False)
+    Customer_Gender=models.CharField(max_length=255)
+    Customer_Income_Group=models.CharField(max_length=255)
+    Customer_Region=models.CharField(max_length=255)
+    Customer_Marital_Status=models.BooleanField(default=False)
--- profiles_api/views.py
+++ profiles_api/views.py
@@ -3,6 +3,8 @@ from rest_framework.response import Response
 from rest_framework import status
 from profiles_api import serializers
 from rest_framework import viewsets
+from profiles_api import models
+from .models import Policy
 
 class HelloApiView(APIView):
     """Test API View"""
@@ -87,3 +89,13 @@ class HelloViewSet(viewsets.ViewSet):
     def destroy(self,request,pk=None):
         """Handle removing an object """
         return Response({{'http_method':'DELETE'}})
+
+class UserProfileViewSet(viewsets.ModelViewSet):
+    """Handle creating and updating profiles"""
+    serializer_class=serializers.UserProfileSerializer
+    queryset=models.UserProfile.objects.all()
+
+class PolicyViewSet(viewsets.ModelViewSet):
+    """Handle creating and updating policies"""
+    serializer_class=serializers.PolicySerializer
+    queryset=Policy.objects.all()
